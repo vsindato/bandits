98fe7779e56cf176b8bef87b2d1a9c8f442ecb37
Victor Sindato <victor@Victors-MacBook-Pro.local>
2021-03-06 15:33:52 -0500

    Implement and document constructor and sample methods

--- env/distributions/bernoulli.py
+++ env/distributions/bernoulli.py
@@ -1,25 +1,37 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import math
 
 
 class Bernoulli:
-    """Bernoulli distribution type
+    """Bernoulli distribution
 
-    TODO #1: define bernoulli
-         #2: explain the methods
+    TODO #1: explain the methods
+    A bernoulli distribution, X ~ Bernoulli(p), is a distribution with 
+    2 possible outcomes; a 0 ("failure") and 1("success").The parameter p
+    represents the probability of "success".
+
+    Methods
+    -------
+    #TODO: Explain the methods
+
     """
 
-    def __init__(self, p):
-        """summary
+    def __init__(self, p: float) -> None:
+        """Constructor method
 
+        Parameters
+        ----------
+        p: probability of "success" i.e. outcome = 1
         """
 
         self.p = p
 
-    def sample(self):
-        """summary
+    def sample(self) -> int:
+        """Sampling method
+
+        Output
+        ------
+        A value of 0/1
 
         """
 
-        pass
+        return np.random.binomial(1, self.p)
